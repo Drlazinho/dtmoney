59874c8bf464addc215a7f1751acc918dfac0e0f
Drlazinho <lazbonfim@hotmail.com>
2022-03-01 12:49:37 -0300

    New creation for context

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -11,11 +11,33 @@ interface Transaction {
   createdAt: string
 }
 
+//Formas de tipagem
+
+// interface TransactionInput {
+//   title: string
+//   amount: number
+//   type: string
+//   category: string
+// }
+
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'> //Passando todas as proprieades do Transaction menos id e createdAt
+
+//type TransactionInput = Pick<Transaction, 'title' | 'amount' | 'category' | 'type'> //Passando as props selecionadas
+
 interface TransactionsProviderProps {
   children: ReactNode; //Aceita qualquer elemento filho no react
 }
 
-export const TransactionsContext = createContext<Transaction[]>([]);
+interface TransactionsContextData {
+  transactions: Transaction[];
+  createTransaction: (transaction: TransactionInput) => void;
+}
+
+export const TransactionsContext = createContext<TransactionsContextData>(
+  {} as TransactionsContextData
+  //É um erro pra driblar o typescript, não vai ser utilizado, e vai substituido
+  
+  );
 
 export function TransactionsProvider({children}: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([])
@@ -26,8 +48,13 @@ export function TransactionsProvider({children}: TransactionsProviderProps) {
       .then(response => setTransactions(response.data.transactions))
   }, []);
 
+  function createTransaction(transaction : TransactionInput) {
+
+    api.post('/transactions', transaction)
+  }
+
   return (
-    <TransactionsContext.Provider value={transactions}>
+    <TransactionsContext.Provider value={{transactions, createTransaction}}>
       {children}
     </TransactionsContext.Provider>
   )
